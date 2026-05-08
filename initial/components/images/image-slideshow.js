2f682a14ba8d7e7907e2812d6ff47ae4b4e7befa
Rajal7777 <suwalrajal57@gmail.com>
2026-05-08 16:00:41 +0900

    Image compoent and used 'use client'

--- initial/components/images/image-slideshow.js
+++ initial/components/images/image-slideshow.js
@@ -2,14 +2,15 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import classes from "./image-slideshow.module.css";
 
-import burgerImg from "@/assets/burger.png";
-import curryImg from "@/assets/curry.png";
-import pizzaImg from "@/assets/pizza.png";
-import dumplingsImg from "@/assets/dumplings.png";
-import macncheeseImg from "@/assets/macncheese.png";
-import schnitzenImg from "@/assets/schnitzen.png";
-import tomatoSaladImg from "@/assets/tomatosalad.png";
+import burgerImg from "@/assets/burger.jpg";
+import curryImg from "@/assets/curry.jpg";
+import pizzaImg from "@/assets/pizza.jpg";
+import dumplingsImg from "@/assets/dumplings.jpg";
+import macncheeseImg from "@/assets/macncheese.jpg";
+import schnitzelImg from "@/assets/schnitzel.jpg";
+import tomatoSaladImg from "@/assets/tomato-salad.jpg";
 
 const images = [
   { image: burgerImg, alt: "Burger" },
@@ -17,13 +18,14 @@ const images = [
   { image: pizzaImg, alt: "Pizza" },
   { image: dumplingsImg, alt: "Dumplings" },
   { image: macncheeseImg, alt: "Mac and Cheese" },
-  { image: schnitzenImg, alt: "Schnitzen" },
+  { image: schnitzelImg, alt: "Schnitzel" },
   { image: tomatoSaladImg, alt: "Tomato Salad" },
 ];
 
 export default function ImageSlideshow() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+  //runs setInterval on component mount and clears on unmount.
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) =>
@@ -34,5 +36,16 @@ export default function ImageSlideshow() {
     return () => clearInterval(interval);
   }, []);
 
-  
+  return (
+    <div className={classes.slideshow}>
+    {images.map((image, index) => (
+        <Image 
+         key={index}
+         src={image.image}
+         alt={image.alt}
+         className={index === currentImageIndex ? classes.active : ''}
+        />
+    )   )}
+  </div>
+  )
 }
