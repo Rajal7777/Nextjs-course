6e1a1852f84a99baf75cffd3d132fa4c980c2905
Rajal7777 <suwalrajal57@gmail.com>
2026-05-18 20:46:28 +0900

    loading.js

--- initial/app/meals/page.js
+++ initial/app/meals/page.js
@@ -7,8 +7,7 @@ import { getMeals } from "@/lib/meals";
 export default async function MealsPage(){
   const meals = await getMeals();
 
-  console.log('meals', meals)
-
+ 
     return(
         <>
         <header className={classes.header}>
--- initial/lib/meals.js
+++ initial/lib/meals.js
@@ -2,7 +2,7 @@ import Database from "better-sqlite3";
 
 const db = new Database("meals.db");
 
-
 export async function getMeals() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   return db.prepare("SELECT * FROM meals").all();
 }
