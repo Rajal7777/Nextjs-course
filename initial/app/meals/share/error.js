a3a1cefac34eb0f11dd235b0546d464a5ec27e22
Rajal7777 <suwalrajal57@gmail.com>
2026-05-23 14:46:03 +0900

    added server side form validation.

---
+++ initial/app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h2>An error ocurred</h2>
+      <p>Failed to create a meal.</p>
+    </main>
+  );
+}
